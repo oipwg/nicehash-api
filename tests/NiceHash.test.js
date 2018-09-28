105cfc0b608a508a799ba5b6aac0ab67066a22fb
orpheus <ryanachacon@gmail.com>
2018-09-28 10:28:48 -0700

    setup tests, dotenv, and jsdoc

---
+++ tests/NiceHash.test.js
@@ -0,0 +1,13 @@
+import NiceHash from '../src/NiceHash'
+
+import { config } from 'dotenv'
+config()
+
+const apiKey = {
+	key: process.env.API_KEY,
+	id: process.env.API_ID
+};
+
+describe('NiceHash', () => {
+
+});
